d79d79c99d4c879df532f42eba4a874e6bf34c39
Chris Hendrix <chendrix1123@gmail.com>
2021-09-04 11:29:40 -0400

    28. Add & Remove Comment Routes

--- models/Post.js
+++ models/Post.js
@@ -17,9 +17,9 @@ const PostSchema = new Schema({
 			text: { type: String, required: true },
 			name: { type: String },
 			avatar: { type: String },
-			date: { type: Date, default: Date.Now },
+			date: { type: Date, default: Date.now() },
 		},
 	],
-	date: { type: Date, default: Date.Now },
+	date: { type: Date, default: Date.now() },
 });
 module.exports = Post = mongoose.model('post', PostSchema);
