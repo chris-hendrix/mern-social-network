1355c39e82b2d615f6023d091d75b8e69f41154d
Chris Hendrix <chendrix1123@gmail.com>
2021-09-05 18:11:34 -0400

    43. Logout & Navbar Links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
 	AUTH_ERROR,
 	LOGIN_SUCCESS,
 	LOGIN_FAIL,
+	LOGOUT,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -84,4 +85,9 @@ export const login = (email, password) => async (dispatch) => {
 			type: LOGIN_FAIL,
 		});
 	}
+};
+
+// logout and clear profile
+export const logout = () => (dispatch) => {
+	dispatch({ type: LOGOUT });
 };
