5f0a82dee98f7704b3167e8e3fbb538698a62a5a
Chris Hendrix <chendrix1123@gmail.com>
2021-09-06 15:35:14 -0400

    45. Profile Reducer & Get Current Profile

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -14,3 +14,7 @@ export const LOGIN_FAIL = 'LOGIN_FAIL';
 
 // logout
 export const LOGOUT = 'LOGOUT';
+
+// dashboard
+export const GET_PROFILE = 'GET_PROFILE';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
