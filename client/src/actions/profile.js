20d55260caabfad5f24b49f3a4aaf7af30bf2fe1
Chris Hendrix <chendrix1123@gmail.com>
2021-09-09 17:52:37 -0400

    50. Add Education & Experiences

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
 
 // get current user's profile
 export const getCurrentProfile = () => async (dispatch) => {
@@ -52,3 +52,57 @@ export const createProfile =
 			});
 		}
 	};
+
+// add experience
+export const addExperience = (formData, history) => async (dispatch) => {
+	try {
+		//TODO setting default is workaround
+		axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+		const res = await axios.put('/api/profile/experience', formData);
+
+		dispatch({
+			type: UPDATE_PROFILE,
+			payload: res.data,
+		});
+
+		dispatch(setAlert('Experience added', 'success'));
+	} catch (err) {
+		const errors = err.response.data.errors;
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+		}
+
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};
+
+// add education
+export const addEducation = (formData, history) => async (dispatch) => {
+	try {
+		//TODO setting default is workaround
+		axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+		const res = await axios.put('/api/profile/education', formData);
+
+		dispatch({
+			type: UPDATE_PROFILE,
+			payload: res.data,
+		});
+
+		dispatch(setAlert('Education added', 'success'));
+	} catch (err) {
+		const errors = err.response.data.errors;
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+		}
+
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -18,4 +18,7 @@ export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 
 // dashboard
 export const GET_PROFILE = 'GET_PROFILE';
+export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const PROFILE_ERROR = 'PROFILE_ERROR';
+
+//
