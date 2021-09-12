db9bfe11d5635590e8946440000f1dabd93136b5
Chris Hendrix <chendrix1123@gmail.com>
2021-09-12 13:10:52 -0400

    59. Displaying Github Repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,9 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -30,6 +33,31 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
 					<div class='profile-grid my-1'>
 						<ProfileTop profile={profile} />
 						<ProfileAbout profile={profile} />
+						<div className='profile-exp bg-white p-2'>
+							<h2 className='text-primary'>Experience</h2>
+							{profile.experience.length > 0 ? (
+								<Fragment>
+									{profile.experience.map((exp) => (
+										<ProfileExperience key={exp._id} experience={exp} />
+									))}
+								</Fragment>
+							) : (
+								<h4>No experience</h4>
+							)}
+						</div>
+						<div className='profile-edu bg-white p-2'>
+							<h2 className='text-primary'>Education</h2>
+							{profile.education.length > 0 ? (
+								<Fragment>
+									{profile.education.map((edu) => (
+										<ProfileEducation key={edu._id} education={edu} />
+									))}
+								</Fragment>
+							) : (
+								<h4>No experience</h4>
+							)}
+						</div>
+						{profile.githubusername && <ProfileGithub username={profile.githubusername} />}
 					</div>
 				</Fragment>
 			)}
